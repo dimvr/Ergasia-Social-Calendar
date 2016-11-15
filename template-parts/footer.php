c3eba58b934791664c24d71e6e7fa2aeb5d9f50c
dimvr <jimmas26@gmail.com>
2016-11-15 15:36:45 +0200

    Κομματια σελιδας

---
+++ template-parts/footer.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="<?php echo $server; ?>/assets/CSS/Bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo $server; ?>/assets/JS/moment.min.js"></script>
+<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.1/fullcalendar.min.js"></script>
+<script type="text/javascript" src="<?php echo $server; ?>/assets/JS/main-scripts.js"></script>
+</html>
