b893879d4fab055938f69cfda187a0b0c94763f4
dimvr <jimmas26@gmail.com>
2016-11-15 15:30:53 +0200

    Αρχικη

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Η αρχική σελίδα της εφαρμογής είναι αυτό το αρχείο.
+ */
+include( 'template-parts/header.php' ); ?>
+<body>
+	<div class="main-content-container">
+		<div class="container">
+			<div class="row">
+				<div class="col-mg-12">
+					<h1>Καλώς ορίσατε στην εφαρμογή Your Calendar.</h1>
+					<p>Κείμενο εδώ για την εφαρμογή.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+<?php
+include( 'template-parts/footer.php' );
